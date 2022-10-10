d2c25b7a9729e1ec8fd9cbf77ac6dea2ce02731b
zakariya6002 <zhassan6002@gmail.com>
2022-10-10 15:54:45 +0200

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,26 @@
-import React from "react";
+import { Button } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
+import { Image } from "@chakra-ui/image";
 import { Stack, Circle, Flex, Box, Text } from "@chakra-ui/layout";
 import { useMediaQuery } from "@chakra-ui/media-query";
+import React from "react";
 
 function Header() {
-  const { colorMode } = useColorMode;
+  const { colorMode } = useColorMode();
   const isDark = colorMode === "dark";
-  const { isNotSmallerScreen } = useMediaQuery("(min-width:600px)");
+
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
   return (
     <Stack>
       <Circle
         position="absolute"
-        bg="blue.800"
+        bg="blue.100"
         opacity="0.1"
         w="300px"
         h="300px"
         alignSelf="flex-end"
-      ></Circle>
+      />
       <Flex
         direction={isNotSmallerScreen ? "row" : "column"}
         spacing="200px"
@@ -25,23 +28,40 @@ function Header() {
         alignSelf="flex-start"
       >
         <Box mt={isNotSmallerScreen ? "0" : 16} align="flex-start">
-          <Text fontSize="5xl">Hi, I am</Text>
+          <Text fontSize="5xl" fontWeight="semibold">
+            Hi, I am
+          </Text>
           <Text
             fontSize="7xl"
-            FontWeight="bold"
-            bgGradient="linear(to-r,cyan.500, blue.600, purple.700)"
+            fontWeight="bold"
+            bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
             bgClip="text"
           >
-            Zakariya Hassan
+            Pawan Kumar
           </Text>
           <Text color={isDark ? "gray.200" : "gray.500"}>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa
+            GDE - Flutter, Firebase. Founder of https://codepur.dev &
+            https://velocityx.dev. Building @frontierdotxyz, YouTuber &
+            Entrepreneur 🗣
           </Text>
-          <button mt={8} colorScheme="blue" onClick={() => window.open("#")}>
-            Browse
-          </button>
+          <Button
+            mt={8}
+            colorScheme="blue"
+            onClick={() => window.open("https://pawan.live")}
+          >
+            Hire Me
+          </Button>
         </Box>
+        <Image
+          alignSelf="center"
+          mt={isNotSmallerScreen ? "0" : "12"}
+          mb={isNotSmallerScreen ? "0" : "12"}
+          borderRadius="full"
+          backgroundColor="transparent"
+          boxShadow="lg"
+          boxSize="300px"
+          src="https://media-exp1.licdn.com/dms/image/D4D03AQF-5TyGGeNKqw/profile-displayphoto-shrink_400_400/0/1665132724504?e=1671062400&v=beta&t=jRhgMAXNCaUFHXu_ib8wKdCPHHh-BNoSmZ4k0th_6vc"
+        />
       </Flex>
     </Stack>
   );
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Icon from "@chakra-ui/icon";
+import { HStack } from "@chakra-ui/layout";
+import { FaFacebookF, FaGoogle, FaSpotify, FaShopify } from "react-icons/fa";
 
 function Socials() {
-  return <div>Socials</div>;
+  return (
+    <HStack spacing="24">
+      <Icon as={FaFacebookF} boxSize="50" />
+      <Icon as={FaGoogle} boxSize="50" />
+      <Icon as={FaSpotify} boxSize="50" />
+      <Icon as={FaShopify} boxSize="50" />
+    </HStack>
+  );
 }
 
 export default Socials;
