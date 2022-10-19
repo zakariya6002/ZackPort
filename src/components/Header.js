4e7b646b415a3d957bc7a8462b25c760676c7a87
zakariya6002 <zhassan6002@gmail.com>
2022-10-19 10:11:53 +0200

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { Image } from "@chakra-ui/image";
-import { Stack, Circle, Flex, Box, Text } from "@chakra-ui/layout";
+import { Stack, Flex, Box, Text } from "@chakra-ui/layout";
 import { useMediaQuery } from "@chakra-ui/media-query";
 import React from "react";
 
