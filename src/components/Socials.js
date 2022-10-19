1aa4849f071717b1cd65f7dca49661f1d372002f
zakariya6002 <zhassan6002@gmail.com>
2022-10-19 10:01:26 +0200

    updated

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -6,14 +6,9 @@ import { useMediaQuery } from "@chakra-ui/media-query";
 import { FaFacebookF, FaGoogle, FaShopify } from "react-icons/fa";
 
 function Socials() {
-  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+  // const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
   return (
-    <HStack
-      spacing="24"
-      direction={isNotSmallerScreen ? "row" : "column"}
-      w="100%"
-      maxWidth={{ base: "100vh", md: "130vh", lg: "130vh", xl: "130vh" }}
-    >
+    <HStack spacing="19">
       <Icon as={FaFacebookF} boxSize="50" />
       <Icon as={FaGoogle} boxSize="50" />
       <Icon as={FaShopify} boxSize="50" />
